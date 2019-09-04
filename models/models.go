c373a1e8bf35bb1928541ccbf573c4b31b6117b4
Clancey <clanceylu@gmail.com>
2019-09-04 14:54:52 +0800

    rebuild mysql connect

--- models/models.go
+++ models/models.go
@@ -1,55 +1,22 @@
 package models
 
 import (
-	"fmt"
 	"time"
 
-	"github.com/ClanceyLu/echo-api/conf"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/ClanceyLu/echo-api/pkg/mysql"
 	"github.com/jinzhu/gorm"
-	"github.com/spf13/viper"
 )
 
 // Model definition
 type Model struct {
-	ID        uint `gorm:"primary_key"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt *time.Time
+	ID        uint       `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time  `json:"createdAt"`
+	UpdatedAt time.Time  `json:"updatedAt"`
+	DeletedAt *time.Time `json:"deletedAt"`
 }
 
-var (
-	dbConf  *viper.Viper
-	appConf *viper.Viper
-	db      *gorm.DB
-)
+var db *gorm.DB
 
 func init() {
-	dbConf = conf.Conf.Sub("db")
-	appConf = conf.Conf.Sub("app")
-	if err := connectDB(); err != nil {
-		panic(fmt.Errorf("Fatal error connect to db: %s", err))
-	}
-}
-
-// connectDB 连接数据库
-func connectDB() error {
-	var (
-		err    error
-		dbType string = dbConf.GetString("type")
-		host   string = dbConf.GetString("host")
-		name   string = dbConf.GetString("name")
-		user   string = dbConf.GetString("user")
-		pwd    string = dbConf.GetString("password")
-	)
-	if db, err = gorm.Open(dbType,
-		fmt.Sprintf("%s:%s@(%s)/%s?charset=utf8&parseTime=True&loc=Local",
-			user, pwd, host, name)); err != nil {
-		return err
-	}
-	db.LogMode(appConf.GetBool("debug"))
-	db.AutoMigrate(&User{})
-	db.DB().SetMaxIdleConns(10)
-	db.DB().SetMaxOpenConns(100)
-	return nil
+	db = mysql.Connect()
 }
---
+++ pkg/mysql/mysql.go
@@ -0,0 +1,34 @@
+package mysql
+
+import (
+	"fmt"
+
+	"github.com/ClanceyLu/echo-api/conf"
+	"github.com/jinzhu/gorm"
+
+	// gorm 依赖
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// Connect 连接数据库，返回 gorm.DB
+func Connect() *gorm.DB {
+	var (
+		appConf   = conf.Conf.Sub("app")
+		mySQLConf = conf.Conf.Sub("mysql")
+		host      = mySQLConf.GetString("host")
+		user      = mySQLConf.GetString("user")
+		password  = mySQLConf.GetString("password")
+		name      = mySQLConf.GetString("name")
+	)
+	db, err := gorm.Open("mysql",
+		fmt.Sprintf("%s:%s@(%s)/%s?charset=utf8&parseTime=True&loc=Local",
+			user, password, host, name))
+	defer db.Close()
+	if err != nil {
+		panic(err)
+	}
+	db.LogMode(appConf.GetBool("debug"))
+	db.DB().SetMaxIdleConns(10)
+	db.DB().SetMaxOpenConns(100)
+	return db
+}
